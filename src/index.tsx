c83ae6c591c4081780d51e8dbe1fd7ae5d8cc786
Shreyas <shreyxs@gmail.com>
2021-06-08 00:05:12 +0530

    bug fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,9 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
-import 'tachyons';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./containers/App";
+import registerServiceWorker from "./registerServiceWorker";
+import "./index.css";
+import "tachyons";
 
-ReactDOM.render( 
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
+ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
 registerServiceWorker();
