ebc693bdcc516ff9207845d2fcf11f60e6dc1167
Shreyas <shreyxs@gmail.com>
2021-06-09 01:29:35 +0530

    Updates

---
+++ precache-manifest.2f33960db54f33f9bbb689d58fda7af8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a1b902951035149108df5c661a9912e",
+    "url": "/robotans/index.html"
+  },
+  {
+    "revision": "09dbea7ee899c6851ad2",
+    "url": "/robotans/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5c012c08cbde81b739be",
+    "url": "/robotans/static/css/main.bec0d19e.chunk.css"
+  },
+  {
+    "revision": "09dbea7ee899c6851ad2",
+    "url": "/robotans/static/js/2.0ef08eeb.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/robotans/static/js/2.0ef08eeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c012c08cbde81b739be",
+    "url": "/robotans/static/js/main.23b04768.chunk.js"
+  },
+  {
+    "revision": "b75025518b9f7eb9397f",
+    "url": "/robotans/static/js/runtime-main.a0cfb8cf.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotans/static/media/SEGA.12a3db8b.woff"
+  }
+]);
